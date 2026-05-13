9f475e32b2082f1db6b13e530433ecc67319201b
Jeff06-coder <loplopado67@gmail.com>
2026-05-13 13:33:29 -0300

    feat: nova pasta e arquivo adicionados sobre boas práticas

---
+++ BootcampLuizalabs_Back-end/Ambiente_virtual/boas_praticas.py
@@ -0,0 +1,17 @@
+# Em ordem alfabetica as importações
+import os
+import sys
+
+
+
+frutas = [
+    "banana",
+    "maçã", 
+    "laranja", 
+    "manga", 
+    "panga", 
+    "chuchu",
+    ]
+
+d = "123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899100"
+print("Deixar uma linha de código ate 100 caracteres, para facilitar a leitura e manutenção do código "+ d)
